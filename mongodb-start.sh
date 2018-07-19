4c60ba0e28ba41e2d1de1fb810daf568e5cbd016
fsamir <franklin@dattein.com>
2018-07-19 15:16:33 +1000

    don't fail when mongod.lock doesn't exist.

--- mongodb-start.sh
+++ mongodb-start.sh
@@ -3,6 +3,6 @@ set -e
 
 trap 'kill -2 1; wait 1' SIGTERM
 
-rm /data/db/mongod.lock
-mongod --dbpath /data/db --repair
+rm /data/db/mongod.lock || true
+mongod --dbpath /data/db --repair || true
 mongod -f /etc/mongod.conf
